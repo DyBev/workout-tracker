bb9d7505b3809b8bb210273b75805c4bff12f62a
dybev <54762882+DyBev@users.noreply.github.com>
2026-03-14 12:31:02 +0000

    chore(constants): uptaded confirm sign-up colors

--- src/constants/colors.tsx
+++ src/constants/colors.tsx
@@ -1,8 +1,10 @@
 
 export const colors = {
   primary: {
+    black: '#000000',
     white: '#ffffff',
     blue: '#0f62fe',
+    green: '#198038',
     grey: '#9ca3af',
     greyLight: '#d7dadf',
     greyDark: '#717c8e',
